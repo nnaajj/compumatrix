114712b0363836d8e4438bf79921936202f1430e
Jann Chlaryz Ventura <jchlaryz@gmail.com>
2025-02-27 14:10:52 +0800

    compumatrix
    requests

---
+++ index.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compumatrix Requests</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <header class="header">
+        <div class="logo">
+            <img src="" alt="" class="">
+            <span class="logo-text">COMPUMATRIX</span>
+        </div>
+    </header>
+    <div class="container">
+        <div class="sidebar">
+            <h2>MENU</h2>
+            <ul>
+                <li><a href="#">Dashboards</a></li>
+                <li><a href="#">Requests</a></li>
+                <li><a href="#">Users</a></li>
+                <li><a href="#">Inventory</a></li>
+                <li><a href="#">Transactions</a></li>
+                <li><a href="#">Admin</a></li>
+                <li><a href="#">Logs</a></li>
+            </ul>
+        </div>
+        <div class="content">
+        <div class="col-span-12 lg:col-span-6">
+                        <div class="box">
+                            <div class="box-header">
+                                <h5 class="box-title">Requests</h5>
+                            </div>
+                            <div class="box-body">
+                                <div class="table-bordered rounded-sm overflow-auto">
+                                <table class="ti-custom-table ti-custom-table-head ti-head-info">
+                                    <thead class="">
+                                    <tr class="!divide-info/10 dark:!divide-info/10 ">
+                                        <th scope="col" class="!text-gray-800  dark:!text-white/70">Name</th>
+                                        <th scope="col" class="!text-gray-800  dark:!text-white/70">Age</th>
+                                        <th scope="col" class="!text-gray-800  dark:!text-white/70">Address</th>
+                                        <th scope="col" class="!text-end !text-gray-800  dark:!text-white/70">Action</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <tr>
+                                        <td class="text-end font-medium">John Brown</td>
+                                        <td>45</td>
+                                        <td>New York No. 1 Lake Park</td>
+                                        <td class="text-end font-medium">
+                                        <button class="accept-btn">Accept</button>
+                                        <button class="delete-btn">Delete</button>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td class="text-end font-medium">Jim Green</td>
+                                        <td>27</td>
+                                        <td>London No. 1 Lake Park</td>
+                                        <td class="text-end font-medium">
+                                        <button class="accept-btn">Accept</button>
+                                        <button class="delete-btn">Delete</button>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                        <td class="text-end font-medium">Joe Black</td>
+                                        <td>31</td>
+                                        <td>Sidney No. 1 Lake Park</td>
+                                        <td class="text-end font-medium">
+                                        <button class="accept-btn">Accept</button>
+                                        <button class="delete-btn">Delete</button>
+                                        </td>
+                                    </tr>
+                                    </tbody>
+                                </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+</body>
+</html>
